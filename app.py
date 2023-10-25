9be104f33822cdf88178c5b324b7086e659a0ed6
ilhamafifah <148410609+ilhamafifah@users.noreply.github.com>
2023-10-25 13:59:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ def main():
   
 if __name__ == "__main__":
     main()
+
+st.title("Counter Example")
+if "count" not in st.session_state: # (C)
+    st.session_state.count = 0 # (A)
+
+increment = st.button("Increment") # (B)
+if increment:
+    st.session_state.count += 1 # (A)
+
+st.write("Count = ", st.session_state.count) # (A)
