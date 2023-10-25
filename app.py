c3d9ff36de4504a717b9f2faa6c219db997afe20
ilhamafifah <148410609+ilhamafifah@users.noreply.github.com>
2023-10-25 14:19:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ def page1():
 
 def page2():
     st.title("What's your name?")
-    st.text_input("Name", key="name")
+    #st.text_input("Name", key="name")
+    
+    def change_page():
+        st.session_state["page-select"] = "page3"
+
+    with st.form(key="name-form"):
+        st.text_input("Name", key="name")
+        st.form_submit_button(label="Submit", on_click=change_page)
 
 def page3():
+    name = st.session_state["name"]
+    st.title(f"Hello, {name}")
+    
+def page4():
     st.title("Counter Example")
     if "count" not in st.session_state:
         st.session_state.count = 0
@@ -19,15 +30,17 @@ def page3():
     st.write("Count = ", st.session_state.count)
 
 pages = dict(
-    page1="Page 1",
+    page1="Home",
     page2="Page 2",
     page3="Page 3",
+    page4="Counter Example",
 )
 
 page_id = st.sidebar.selectbox( # st.sidebar.*でサイドバーに表示する
     "Page Name",
-    ["page1", "page2", "page3"],
-    #format_func=lambda page_id: pages[page_id], # 描画する項目を日本語に変換
+    ["page1", "page2", "page3", "page4"],
+    format_func=lambda page_id: pages[page_id], # change name that appear
+    key="page-select",
 )
 
 if page_id == "page1":
@@ -38,3 +51,6 @@ if page_id == "page2":
 
 if page_id == "page3":
     page3()
+
+if page_id == "page4":
+    page4()
